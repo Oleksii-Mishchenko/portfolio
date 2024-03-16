dc14175223ee2e5d12b8456bfe461ac4f31e0c97
OleksiiMishchenko <alex.mishch@gmail.com>
2024-03-16 18:30:25 +0200

    Created Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import './logo.scss';
+
+export const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="logo__name">oleksii</span>
+      <span className="logo__surname">mishchenko</span>
+    </div>
+  );
+};
