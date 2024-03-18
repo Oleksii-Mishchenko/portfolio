1e03a9f4062b079c156b15cb10dd636c3642ceee
OleksiiMishchenko <alex.mishch@gmail.com>
2024-03-18 12:42:03 +0200

    --amend

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,25 +5,23 @@ import './hero.scss';
 
 export const Hero = () => {
   return (
-    <>
-      <Container className="container main__container" hasLine>
-        <section className="hero">
-          <div className="hero__info">
-            <h1 className="hero__title">{'hi, i am \n oleksii mishchenko.'}</h1>
+    <Container className="container main__container" hasLine>
+      <section className="hero">
+        <div className="hero__info">
+          <h1 className="hero__title">{'hi, i am \n oleksii mishchenko.'}</h1>
 
-            <p className="hero__description">
-              I am a Kyiv based front-end developer with a focus on creating
-              fast, accessible, visually appealing and responsive websites.
-            </p>
+          <p className="hero__description">
+            I am a Kyiv based front-end developer with a focus on creating fast,
+            accessible, visually appealing and responsive websites.
+          </p>
 
-            <MainButton text="Contact Me" />
-          </div>
+          <MainButton text="Contact Me" />
+        </div>
 
-          <div className="hero__photo">
-            <img src={photo} alt="My portrait" className="hero__portrait" />
-          </div>
-        </section>
-      </Container>
-    </>
+        <div className="hero__photo">
+          <img src={photo} alt="My portrait" className="hero__portrait" />
+        </div>
+      </section>
+    </Container>
   );
 };
