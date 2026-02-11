0920c904f5ad4d60c51e2a9bf4a74c7b264d4f82
OleksiiMishchenko <alex.mishch@gmail.com>
2026-02-11 13:53:33 +0200

    Add skills

--- src/assets/skills/skills.ts
+++ src/assets/skills/skills.ts
@@ -3,14 +3,19 @@
 import CSS from '../images/skills/css.svg?react';
 import HTML from '../images/skills/html.svg?react';
 import JS from '../images/skills/js.svg?react';
-import React from '../images/skills/react.svg?react';
-import Redux from '../images/skills/redux.svg?react';
 import Typescript from '../images/skills/typescript.svg?react';
+import React from '../images/skills/react.svg?react';
+import NextJS from '../images/skills/next-js.svg?react';
+import Redux from '../images/skills/redux.svg?react';
+import Jotai from '../images/skills/jotai.svg?react';
 import Sass from '../images/skills/sass.svg?react';
-import VSCode from '../images/skills/vscode.svg?react';
-import Git from '../images/skills/git.svg?react';
+import Firebase from '../images/skills/firebase.svg?react';
+import Jest from '../images/skills/jest.svg?react';
+import Cypress from '../images/skills/cypress.svg?react';
+import RHF from '../images/skills/react-hook-form.svg?react';
 import Github from '../images/skills/github.svg?react';
 import Figma from '../images/skills/figma.svg?react';
+import Tailwind from '../images/skills/tailwind.svg?react';
 import { ISkill } from '../../types/Skill';
 
 export const skills: ISkill[] = [
@@ -23,32 +28,52 @@ export const skills: ISkill[] = [
     label: 'CSS',
   },
   {
-    image: JS,
-    label: 'Javascript',
+    image: Tailwind,
+    label: 'Tailwind CSS',
   },
   {
-    image: React,
-    label: 'React',
+    image: JS,
+    label: 'Javascript',
   },
   {
     image: Typescript,
     label: 'Typescript',
   },
   {
+    image: React,
+    label: 'React',
+  },
+  {
+    image: NextJS,
+    label: 'Next.js',
+  },
+  {
     image: Redux,
     label: 'Redux',
+  },
+  {
+    image: Jotai,
+    label: 'Jotai',
   },
   {
     image: Sass,
     label: 'Sass',
   },
   {
-    image: VSCode,
-    label: 'VS Code',
+    image: Firebase,
+    label: 'Firebase',
   },
   {
-    image: Git,
-    label: 'Git',
+    image: Jest,
+    label: 'Jest',
+  },
+  {
+    image: Cypress,
+    label: 'Cypress',
+  },
+  {
+    image: RHF,
+    label: 'React hook form',
   },
   {
     image: Github,
